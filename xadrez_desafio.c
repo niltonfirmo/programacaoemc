9a1258b6de72cde971a299a09f48f9ff84fb3ad1
niltonfirmo <148794376+niltonfirmo@users.noreply.github.com>
2025-04-16 20:53:38 +0000

    xadrez desafio

--- xadrez_desafio.c
+++ xadrez_desafio.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    printf("*** XADREZ ***\n\n");
+    // Movimento da Torre
+    printf("Movimento da Torre:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Direita\n");
+    }
+
+    // Movimento do Bispo
+    printf("\nMovimento do Bispo:\n");
+    int bispo = 0;
+    while (bispo < 5) {
+        printf("Cima e Direita\n");
+        bispo++;
+    }
+
+    // Movimento da Rainha
+    
+    printf("\nMovimento da Rainha:\n");
+    int rainha = 0;
+    do {
+        printf("Esquerda\n");
+        rainha++;
+    } while (rainha < 8);
+
+    printf("\n*** XADREZ ***\n");
+
+    return 0;
+}
 
