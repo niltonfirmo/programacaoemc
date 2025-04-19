dac8da56fb14ff3876993a2a6c54602ade347581
niltonfirmo <148794376+niltonfirmo@users.noreply.github.com>
2025-04-19 00:18:59 +0000

    criando tabuleiro

--- batalha_naval.c
+++ batalha_naval.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+//batalha naval, 0 água e 3 navios
+ 
+int main() {
+    // Definindo o tabuleiro 10x10 e inicializando todas as posições 
+    int tabuleiro[10][10] = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 3, 3, 3, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    
+  
+
+   for (int i = 0; i < 10; i++ ) {
+      for(int j = 0; j < 10; j++){
+    //exibindo o tabuleiro
+    printf( "%d ", tabuleiro [i][j]);
+   }
+    printf("\n");
+   }
+   // pular duas linhas depois do tabuleiro melhor visualização
+   printf("\n\n");
+       
+ 
+    return 0;
+}
